72f89e2536719d1a650665bca80d35f34b1c2303
Pedro Ricardo <70300680+phricardo@users.noreply.github.com>
2024-12-02 17:29:08 -0300

    chore: update page.tsx v15

--- src/app/[link]/page.tsx
+++ src/app/[link]/page.tsx
@@ -2,11 +2,11 @@ import { notFound, redirect } from "next/navigation";
 import { getRedirectUrl } from "../links";
 
 interface RedirectPageProps {
-  params: { link: string }; 
+  params: Promise<{ link: string }>; 
 }
 
 export default async function RedirectPage({ params }: RedirectPageProps) {
-  const { link: linkKey } = params;
+  const { link: linkKey } = await params;
   const redirectUrl = getRedirectUrl(linkKey);
 
   if (redirectUrl) {
